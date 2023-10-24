47421c77fe7c7c338d85e3461192e6003317c05d
Irina <sidoren-irina@yandex.ru>
2023-10-24 22:28:29 +0300

    Article list block style

--- components/blocks/ArticleListBlock.vue
+++ components/blocks/ArticleListBlock.vue
@@ -2,7 +2,7 @@
 .article-list(v-if="content")
   .article-list__card(v-for="article in content.articles")
     .article-list__card-img-wr
-      img.article-list__card-img(:src="article.image")
+      img.article-list__card-img(:src="article.image" loading="lazy" :alt="article.title")
     h3.article-list__card-title {{article.title}}
     NuxtLink.article-list__card-btn(:to="article.link") Читать
 </template>
@@ -30,6 +30,7 @@ defineProps(['content'])
 
 	&__card-img-wr {
 		height: 320px;
+		background: $gray;
 	}
 
 	&__card-img {
@@ -39,6 +40,17 @@ defineProps(['content'])
 
 	&__card-title {
 		margin: 20px 0 30px;
+		@include font(22, 30, 800);
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+		height: toRem(60);
+	}
+
+	&__card-btn {
+		@extend %buttonBlack;
 	}
 }
 </style>
